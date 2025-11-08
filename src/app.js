e8e8c615131b8a58f86b5f11cdf8aee5e882cc3d
Omar Echaboukh <oechabouk@gmail.com>
2025-11-08 19:38:48 +0000

    Actualizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+document.addEventListener('DOMContentLoaded'), () => {
+  const partesDominio = {
+    prefijos: [
+      "super", "mega", "ultra", "hyper", "pro", "rápido", "fácil", "inteligente",
+      "genial", "digital", "global", "futuro", "mejor", "sólido", "experto"
+    ],
+    sustantivos: [
+      "web", "app", "solución", "software", "idea", "creador", "guía", "foco",
+      "proyecto", "tienda", "base", "nube", "mundo", "maestro", "ruta"
+    ],
+    sufijos: [
+      "ahora", "online", "ya", "plus", "hub", "lab", "io", "tech",
+      "box", "go", "net", "top", "store", "zone", "up"
+    ],
+  };
+  const extensiones = [".com",".es",".net",".io",".app",".nl",".us","uk"];
+  const dominioElemento = document.getElementById("dominio-generado");
+  const botonGenerar = document.getElementById("boton-generador");
+
+  function obtenerDominio(array){
+    const indice = Math.floor(Math.random() * array.length);
+  }
+}
